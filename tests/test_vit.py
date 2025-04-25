10cee4793dc4ccf01c778b3bca95d7489bba6ab5
Scott Chase Waggener <chase.waggener@icloud.com>
2025-04-25 13:07:28 -0500

    Support MLP head (#16)

--- tests/test_vit.py
+++ tests/test_vit.py
@@ -217,7 +217,7 @@ class TestViT:
 
     @pytest.mark.parametrize("backend", ["pytorch", "te"])
     @pytest.mark.parametrize("mlp", [False, True])
-    def test_forward_head(self, backend, config, mlp):
+    def test_forward_head_no_pooling(self, backend, config, mlp):
         if backend == "te" and te is None:
             pytest.skip("Transformer Engine is not available")
         config = replace(config, backend=backend)
@@ -225,9 +225,27 @@ class TestViT:
 
         x = torch.randn(1, 3, 224, 224, device=device)
         model = ViT(config).to(device)
-        head = model.create_mlp(1) if mlp else model.create_head(1)
+        head = model.create_head(1, mlp=mlp)
         head = head.to(device)
         with torch.autocast(device_type=device, dtype=torch.bfloat16, enabled=True):
             out, cls_token = model(x)
             out = head(cls_token)
+        assert out.shape == (1, 1)
+
+    @pytest.mark.parametrize("backend", ["pytorch", "te"])
+    @pytest.mark.parametrize("mlp", [False, True])
+    @pytest.mark.parametrize("pool_type", ["avg", "max"])
+    def test_forward_head_pooling(self, backend, config, mlp, pool_type):
+        if backend == "te" and te is None:
+            pytest.skip("Transformer Engine is not available")
+        config = replace(config, backend=backend)
+        device = "cuda" if backend == "te" else "cpu"
+
+        x = torch.randn(1, 3, 224, 224, device=device)
+        model = ViT(config).to(device)
+        head = model.create_head(1, mlp=mlp, pool_type=pool_type)
+        head = head.to(device)
+        with torch.autocast(device_type=device, dtype=torch.bfloat16, enabled=True):
+            out, _ = model(x)
+            out = head(out)
         assert out.shape == (1, 1)
