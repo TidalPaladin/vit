da8a9958c7942e6ee0e9dda9adbcf25cfba5f684
Scott Chase Waggener <chase.waggener@icloud.com>
2025-05-01 01:07:32 +0000

    Use MLP in position encoding

--- vit/patch_embed.py
+++ vit/patch_embed.py
@@ -30,27 +30,36 @@ class PatchEmbed2d(nn.Module):
     def __init__(
         self,
         in_channels: int,
-        embed_dim: int,
+        hidden_size: int,
+        ffn_hidden_size: int,
         patch_size: Sequence[int],
         normalization: str = "LayerNorm",
+        activation: str = "gelu",
         backend: Backend = DEFAULT_BACKEND,
         eps: float = 1e-5,
     ):
         super().__init__()
         self._patch_size = tuple(patch_size)
-        self.patch = nn.Conv2d(in_channels, embed_dim, self.patch_size, stride=self.patch_size)
-        self.pos_enc = RelativeFactorizedPosition(2, embed_dim, backend=backend, eps=eps)
+        self.patch = nn.Conv2d(in_channels, hidden_size, self.patch_size, stride=self.patch_size)
+        self.pos_enc = RelativeFactorizedPosition(
+            2,
+            hidden_size,
+            ffn_hidden_size,
+            backend=backend,
+            normalization=normalization,
+            activation=activation,
+        )
         match (normalization, backend):
             case ("LayerNorm", "pytorch"):
-                self.norm = nn.LayerNorm(embed_dim, eps=eps)
+                self.norm = nn.LayerNorm(hidden_size, eps=eps)
             case ("RMSNorm", "pytorch"):
-                self.norm = nn.RMSNorm(embed_dim, eps=eps)
+                self.norm = nn.RMSNorm(hidden_size, eps=eps)
             case ("LayerNorm", "te"):
                 check_te_installed(te)
-                self.norm = te.LayerNorm(embed_dim, eps=eps)
+                self.norm = te.LayerNorm(hidden_size, eps=eps)
             case ("RMSNorm", "te"):
                 check_te_installed(te)
-                self.norm = te.RMSNorm(embed_dim, eps=eps)
+                self.norm = te.RMSNorm(hidden_size, eps=eps)
             case _:
                 raise ValueError(f"Invalid normalization: {normalization}")
 
@@ -84,16 +93,18 @@ class ConvNextPatchEmbed2d(PatchEmbed2d):
     def __init__(
         self,
         in_channels: int,
-        embed_dim: int,
+        hidden_size: int,
+        ffn_hidden_size: int,
         patch_size: Sequence[int],
         normalization: str = "LayerNorm",
+        activation: str = "srelu",
         backend: Backend = DEFAULT_BACKEND,
         eps: float = 1e-5,
         depth: int = 2,
         convnext_patch_size: Sequence[int] = [2, 2],
         **kwargs,
     ):
-        super().__init__(in_channels, embed_dim, patch_size, normalization, backend, eps)
+        super().__init__(in_channels, hidden_size, ffn_hidden_size, patch_size, normalization, activation, backend, eps)
         check_convnext_installed(convnext)
         assert len(set(patch_size)) == 1, "Patch size must be the same for all dimensions"
         assert all(p % 2 == 0 for p in patch_size), "Patch size must be even"
@@ -108,7 +119,7 @@ class ConvNextPatchEmbed2d(PatchEmbed2d):
         depths = [depth] * needed_levels
 
         # Increment the width of each level by 2x, matching embed_dim on the last level
-        hidden_sizes = list(reversed([embed_dim // (2 ** (i + 1)) for i in range(needed_levels)]))
+        hidden_sizes = list(reversed([hidden_size // (2 ** (i + 1)) for i in range(needed_levels)]))
         ffn_hidden_sizes = [d * 4 for d in hidden_sizes]
 
         # NOTE: This approach allows leakage between adjacent tokens, which may impact masked image modeling tasks.
@@ -127,7 +138,7 @@ class ConvNextPatchEmbed2d(PatchEmbed2d):
         )
         config = replace(config, **kwargs)
         self.patch = config.instantiate()
-        self.final_conv = nn.Conv2d(hidden_sizes[-1], embed_dim, 2, stride=2)
+        self.final_conv = nn.Conv2d(hidden_sizes[-1], hidden_size, 2, stride=2)
 
     def forward(self, x: Tensor, additional_features: Tensor | None = None) -> Tensor:
         P1, P2 = self.patch_size
