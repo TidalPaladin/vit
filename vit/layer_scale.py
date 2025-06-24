857344200eab4dd3c2feb52a31dacebadb103981
Scott Chase Waggener <chase.waggener@icloud.com>
2025-06-24 21:04:43 +0000

    Support LayerScale

---
+++ vit/layer_scale.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+@torch.compile(fullgraph=True)
+def layer_scale(x: Tensor, gamma: Tensor, inplace: bool = False) -> Tensor:
+    if inplace and not x.requires_grad:
+        return x.mul_(gamma)
+    else:
+        return x * gamma
+
+
+class LayerScale(nn.Module):
+    def __init__(self, dim: int, init_value: float = 1e-5, inplace: bool = False):
+        super().__init__()
+        self.inplace = inplace
+        self.gamma = nn.Parameter(torch.empty(dim))
+        self.reset_parameters(init_value)
+
+    def reset_parameters(self, value: float = 1e-5):
+        nn.init.constant_(self.gamma, value)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return layer_scale(x, self.gamma, self.inplace)
