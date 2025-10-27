03698ee36191ccf3bedb17026100e7f1dbb16e74
Scott Chase Waggener <chase.waggener@icloud.com>
2025-10-27 15:27:40 -0500

    Allow output norm to be disabled in forward

--- vit/_version.py
+++ vit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.1.dev8+g7485062de.d20251015'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev8', 'g7485062de.d20251015')
+__version__ = version = '0.1.1.dev8+gecc3dc8fc.d20251027'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev8', 'gecc3dc8fc.d20251027')
 
 __commit_id__ = commit_id = None
